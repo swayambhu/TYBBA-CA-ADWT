bdd434435e168cbad251a7810420f9d123c3835b
swayambhu <swayambhu43@gmail.com>
2021-05-15 08:59:56 +0530

    Solved error in Conversion

--- 00 SLIPS/01 Slip 1/slip1(a).php
+++ 00 SLIPS/01 Slip 1/slip1(a).php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -23,35 +24,37 @@
 </body>
 </html>
 
+
 <?php
 if(isset($_POST['submit']))
 {
-$no1=$_POST['no1'];
-$no2=$_POST['no2'];
-$cal=$_POST['cal'];
-if((!empty($no1)) && (!empty($no2)))
-{
-switch($cal)
-{
-case 1:$add=$no1+$no2;
-echo "<h1>addition=".$add."</h1>";
-break;
+    $no1=$_POST['no1'];
+    $no2=$_POST['no2'];
+    $cal=$_POST['cal'];
+    if((!empty($no1)) && (!empty($no2)))
+    {
+        switch($cal)
+        {
+            case 1:$add=$no1+$no2;
+                echo "<h1>addition=".$add."</h1>";
+                break;
 
-case 2:$sub=$no1-$no2;
-echo "<h1>subtraction=".$sub."</h1>";
-break;
+            case 2:$sub=$no1-$no2;
+                echo "<h1>subtraction=".$sub."</h1>";
+                break;
 
-case 3:$mult=$no1*$no2;
-echo "<h1>multiplication=".$mult."</h1>"; break;
+            case 3:$mult=$no1*$no2;
+                echo "<h1>multiplication=".$mult."</h1>"; 
+                break;
 
-case 4:$div=$no1/$no2;
-echo "<h1>division=".$div."</h1>";
-break;
+            case 4:$div=$no1/$no2;
+                echo "<h1>division=".$div."</h1>";
+                break;
 
-}
-}
-else echo "Please enter both 2 nos";
+        }
+    }
+    else echo "Please enter both 2 nos";
 }
 else
-die("not able to acccess");
+    die("");
 ?>
--- 06. Converesion/converstion.php
+++ 06. Converesion/converstion.php
@@ -1,26 +1,27 @@
-<!-- @format -->
-
 <html>
-  <head
-    ><title>Temperature Conversion</title></head
-  >
-  <body>
-    <?php
-      $fahr = $_GET['fahrenheit'];
-      if (is_null($fahr)) {
-        ?>
+<head><title>Temperature Conversion</title></head>
+<body>
+  
+<?php
 
-        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="GET">
-          Fahrenheit temperature:
-          <input type="text" name="fahrenheit" /> <br />
-          <input type="submit" name="Convert to Celsius!" />
-        </form>
+  
+  if (empty($_GET['fahrenheit']) OR $_GET['fahrenheit']== 'undefined') {
+  ?>
+  
+  <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="GET">
+  Fahrenheit temperature:
+  <input type="text" name="fahrenheit" /> <br />
+  <input type="submit" name="Convert_to_Celsius!" />
+  </form>
+  
+  <?php
+  } else {
+    $fahr = $_GET['fahrenheit'];
+    $celsius = ($fahr - 32) * 5/9;
+    printf("%.2fF is %.2fC", $fahr, $celsius);
+  }
 
-        <?php
-      } else {
-        $celsius = ($fahr - 32) * 5/9;
-        printf("%.2fF is %.2fC", $fahr, $celsius);
-      }
-    ?>
-  </body>
+?>
+  
+</body>
 </html>
