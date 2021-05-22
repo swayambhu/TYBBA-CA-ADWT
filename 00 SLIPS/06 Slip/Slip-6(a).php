7c087729eba7e116b98c1b814ba6d196499d6e01
swayambhu <swayambhu43@gmail.com>
2021-05-22 13:12:37 +0530

    Error solved

---
+++ 00 SLIPS/06 Slip/Slip-6(a).php
@@ -0,0 +1,43 @@
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    
+<?php
+if(isset($_POST['Login'])){
+$u=$_POST['txtuid'];
+$p=$_POST['txtpwd'];
+$uid='root';
+$pwd='';
+$db='testdb';
+
+$con=mysqli_connect('localhost',$uid,$pwd,$db) or die("wrong");
+
+echo "Great work connection established!";
+
+$sql="Select * from Login where uid='$u' and pwd='$p'";
+$r=mysqli_query($con,$sql);
+$n=mysqli_num_rows($r);
+
+if($n >= 1){
+echo "<h3><span style='color:blue'>Login Successfully</span></h3>";
+
+}else{
+echo "<h3><span style='color:red'>Login Denied</span></h3>";
+}
+mysqli_close($con);
+}
+?>
+
+<form name="f1"action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST"  >
+
+<b>USERNAME :</b><br/>
+<input type="text" name="txtuid"><br/>
+<b>PASSWORD :</b><br/>
+<input type="password" name="txtpwd"><br/>
+<input type="reset"  value="RESET"> 
+<input type="submit" name="Login" value="SUBMIT">
+</form>
+</body>
+</html>
