2a3aa6fddd63af6f6a78ff24b3088c14d63f00f6
swayambhu <swayambhu43@gmail.com>
2021-06-22 12:04:46 +0530

    Slip 7b added

--- 00 SLIPS/07 Slip 7/Slip 7 b/slip7b.php
+++ 00 SLIPS/07 Slip 7/Slip 7 b/slip7b.php
@@ -1,7 +1,7 @@
 <?php
 $uid='root';
 $pwd='';
-$db='testdb';
+$db='db';
 $bnm = $_GET['b'];
 $con=mysqli_connect('localhost',$uid,$pwd,$db) or die("Wrong ");
  echo "Great work!!";
@@ -11,16 +11,22 @@ $con=mysqli_connect('localhost',$uid,$pwd,$db) or die("Wrong ");
     $n = mysqli_num_rows($r);
     
     echo "<table border = 2>";
-    echo "<tr> <th> Bno </th> <th> Name </th>";
-    echo "<th> Source </th><th> Destination </th></tr>";
+    echo "<tr> 
+               <th> Bno </th>
+               <th> Name</th>
+               <th> Source </th>
+               <th> Destination</th>
+          </tr>";
     
     for($i = 0 ; $i < $n; $i++)
     {
          $row = mysqli_fetch_assoc($r);
-         echo "<tr> <td> " . $row["bno"] . "</td>";
-         echo "<td> " . $row["bname"] . "</td>";
-         echo "<td> " . $row["src"] . "</td>";
-         echo "<td> " . $row["des"] . "</td></tr>";
+         echo "<tr> 
+                    <td> " . $row["bno"] . "</td>
+                    <td> " . $row["bname"] . "</td>
+                    <td> " . $row["src"] . "</td>
+                    <td> " . $row["des"] . "</td>
+               </tr>";
          
     }
     mysqli_close($con);
